ee9f524b4c8bcbd9537f2b3c28d3364feb3a7094
AnJiYeong <memarcis2@naver.com>
2022-10-20 13:56:29 +0900

    up

--- projects/w3school_Join/Products_Quantity.sql
+++ projects/w3school_Join/Products_Quantity.sql
@@ -1,4 +1,4 @@
--- +3 : 보여지는 내용 - 제품명, 가격, 주문 개수 나타내기
+-- +3 : 제품명, 가격, 주문 개수 나타내기
 SELECT Products.ProductName, Products.Price, OrderDetails.Quantity
 FROM  Products INNER JOIN OrderDetails
 ON Products.ProductID = OrderDetails.ProductID;
